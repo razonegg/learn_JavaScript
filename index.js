5df98256deeea76862f90acd93e2f22cdfd1788a
razone-cmd <tamurajan17@gmail.com>
2024-06-30 06:44:13 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// cost result = 90;
+// console.log(result);
+
+// result = 67;
+// console.log(result);
+
+// const output = "Ram Babu";
+// console.log(output);
+// console.log(type of output)
+
+// const x = 3.65;
+// console.log(typeof x);
+// console.log(x.toFixed(2)); // 3.65 -> 3.65
+
+// let y ;
+// console.log(y); // undefined
+// console.log(typeof y);
+// const istrue = true;
+// console.log(istrue);
+// console.log(typeof istrue); // boolean
+
+//symbol
+// let sym = Symbol();
+const output = Symbol("Hello world");
+console.log(output);
+console.log(typeof output);
+
+
+
+
+
+
+
