36ae8c5a02df1d253ea5c774be4a64204d5dc34b
razone-cmd <tamurajan17@gmail.com>
2024-06-30 07:55:23 +0545

    daySUNDAY

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
-// cost result = 90;
-// console.log(result);
+// // cost result = 90;
+// // console.log(result);
 
-// result = 67;
-// console.log(result);
+// // result = 67;
+// // console.log(result);
 
-// const output = "Ram Babu";
-// console.log(output);
-// console.log(type of output)
+// // const output = "Ram Babu";
+// // console.log(output);
+// // console.log(type of output)
 
-// const x = 3.65;
-// console.log(typeof x);
-// console.log(x.toFixed(2)); // 3.65 -> 3.65
+// // const x = 3.65;
+// // console.log(typeof x);
+// // console.log(x.toFixed(2)); // 3.65 -> 3.65
 
-// let y ;
-// console.log(y); // undefined
-// console.log(typeof y);
-// const istrue = true;
-// console.log(istrue);
-// console.log(typeof istrue); // boolean
+// // let y ;
+// // console.log(y); // undefined
+// // console.log(typeof y);
+// // const istrue = true;
+// // console.log(istrue);
+// // console.log(typeof istrue); // boolean
 
-//symbol
-// let sym = Symbol();
-const output = Symbol("Hello world");
-console.log(output);
-console.log(typeof output);
+// //symbol
+// // let sym = Symbol();
+// // const output = Symbol("Hello world");
+// // console.log(output);
+// // console.log(typeof output);
 
-
-
-
-
+// // const date
+// // console.log(date)
 
 
+// const result = "hello world";
+// //length
+// console.log(result.length);
+
+// // chartAt
+// console.log(result.chartAt(2));
+// //indexof
+// console.log(result.indexOf("o"));
+// //lastIndexOf
+// console.log(result.lastIndexOf("o"));
+// //slice
+// console.log(result.slice(1,6));
+// //concat
+// console.log(result.concat("I LOVE Java"));
+
+// let output = "                     Hello World";
+// console.log(output.trim());
+
+// // split
+// console.log(result.split("  "));
+
+// //concatination
+// // let x = "Hello World";
+// // let y = "I Love Us";
+// // let z = "King Is Bling";
+
+// // console.log(x +" " + y + " " + z);
+
+// const a = 90;
+// const b = 80;
+// const result = a + b ;
+// console.log("The sum of two number is:" + result);
+// console.log("The sum of two number is:" $(result));
+
+
+let y = 90;
+y = y+10;
+console.log(y)
+
+let x = 70;
+x += 50;
+console.log(x)
